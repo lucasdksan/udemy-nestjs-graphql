39df3c95845e1545cb8b838ab95e00c95ac18ec5
lucasdksan <lucas.leoncio.silva@gmail.com>
2024-11-12 00:12:39 -0300

    criando os testes de integração para os casos de uso da entidade posts

--- src/posts/helpers/posts-data-builder.ts
+++ src/posts/helpers/posts-data-builder.ts
@@ -1,10 +1,13 @@
 import { faker } from "@faker-js/faker";
 import { Post } from "../graphql/models/post";
+import slugify from "slugify";
 
 export function PostDataBuilder(props: Partial<Post>): Omit<Post, "id" | "authorId"> {
+    const title = props.title ?? faker.lorem.sentence()
+    
     return {
-        title: props.title ?? faker.lorem.sentence(),
-        slug: props.slug ?? faker.lorem.slug(),
+        title: title,
+        slug: props.slug ?? slugify(title, { lower: true }),
         content: props.content ?? faker.lorem.paragraph(),
         published: props.published ?? false,
         createdAt: props.createdAt ?? new Date(),
---
+++ src/posts/usecases/__tests__/create-post.usecase.int-spec.ts
@@ -0,0 +1,55 @@
+import { AuthorsPrismaRepository } from "@/authors/repositories/authores-prisma.repository";
+import { PostsPrismaRepository } from "@/posts/repositories/posts-prisma.repository";
+import { Test, TestingModule } from "@nestjs/testing";
+import { PrismaClient } from "@prisma/client";
+import { execSync } from "node:child_process";
+import { CreatePost } from "../create-post.usecase";
+import { PostDataBuilder } from "@/posts/helpers/posts-data-builder";
+import { AuthorDataBuilder } from "@/authors/helpers/author-data-builder";
+
+jest.setTimeout(30000); 
+
+describe("CreatePostUsecase Integration tests", ()=> {
+    let module: TestingModule;
+    let postsRepository: PostsPrismaRepository;
+    let authorsRepository: AuthorsPrismaRepository;
+    let usecase: CreatePost.Usecase;
+    const prisma = new PrismaClient();
+
+    beforeAll(async ()=> {
+        execSync("npm run prisma:migrate--test");
+
+        await prisma.$connect();
+
+        module = await Test.createTestingModule({}).compile();
+        postsRepository = new PostsPrismaRepository(prisma as any);
+        authorsRepository = new AuthorsPrismaRepository(prisma as any);
+        usecase = new CreatePost.Usecase(postsRepository, authorsRepository);
+    });
+
+    beforeEach(async ()=> {
+        await prisma.post.deleteMany();
+        await prisma.author.deleteMany();
+    });
+
+    afterAll(async ()=> {
+        await module.close();
+    });
+
+    test("Should create a post", async ()=>{
+        const postData = PostDataBuilder({});
+        const authorData = AuthorDataBuilder({});
+
+        const author = await prisma.author.create({ data: authorData });
+        const post = await usecase.execute({ authorId: author.id, title: postData.title, content: postData.content });
+
+        expect(post.id).toBeDefined();
+        expect(post.createdAt).toBeInstanceOf(Date);
+        expect(post.updatedAt).toBeInstanceOf(Date);
+        expect(post).toMatchObject({
+            title: postData.title,
+            content: postData.content,
+            published: postData.published,
+        });
+    });
+});
